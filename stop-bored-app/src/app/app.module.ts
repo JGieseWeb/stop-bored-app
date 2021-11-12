06c25c603bf27cc166192635b4b27809c5e3b780
jannik <giese.jannik@web.de>
2021-11-12 21:51:27 +0100

    ✨create midbody component and integrate app-activity

--- stop-bored-app/src/app/app.module.ts
+++ stop-bored-app/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MidbodyComponent } from './components/midbody/midbody.component';
+import { ActivityComponent } from './components/activity/activity.component';
+import { ActivityItemsComponent } from './components/activity-items/activity-items.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, HeaderComponent, MidbodyComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    HeaderComponent,
+    MidbodyComponent,
+    ActivityComponent,
+    ActivityItemsComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- stop-bored-app/src/app/components/midbody/midbody.component.ts
+++ stop-bored-app/src/app/components/midbody/midbody.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-midbody',
   templateUrl: './midbody.component.html',
-  styleUrls: ['./midbody.component.css']
+  styleUrls: ['./midbody.component.css'],
 })
 export class MidbodyComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
